0c25a98947278fa35d3be8ac6ba4027601ea47c0
wen <wen.sima@gmail.com>
2024-10-16 23:42:21 +1100

    update edit cabin function

--- src/features/cabins/CreateCabinForm-v1.jsx
+++ src/features/cabins/CreateCabinForm-v1.jsx
@@ -1,28 +1,22 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
-import { toast } from "react-hot-toast";
 
 import Input from "../../ui/Input";
 import Form from "../../ui/Form";
 import Button from "../../ui/Button";
 import FileInput from "../../ui/FileInput";
 import Textarea from "../../ui/Textarea";
-import { createEditCabin } from "../../services/apiCabins";
+import { createCabin } from "../../services/apiCabins";
 import FormRow from "../../ui/FormRow";
 
-function CreateCabinForm({ cabinToEdit = {} }) {
-  const { id: editId, ...editValues } = cabinToEdit;
-  const isEditSession = Boolean(editId);
-
-  const { register, handleSubmit, reset, getValues, formState } = useForm({
-    defaultValues: isEditSession ? editValues : {},
-  });
+function CreateCabinForm({ cabin }) {
+  const { register, handleSubmit, reset, getValues, formState } = useForm();
   const { errors } = formState;
   //console.log(errors);
   const queryClient = useQueryClient();
-  //useMutation 会自动跟踪异步请求的状态，比如isLoading（正在进行中）、isSuccess（请求成功）、isError（请求失败）等。
-  const { mutate: createCabin, isLoading: isCreating } = useMutation({
-    mutationFn: createEditCabin,
+
+  const { mutate, isLoading: isCreating } = useMutation({
+    mutationFn: createCabin,
     onSuccess: () => {
       toast.success("New cabin successfully created");
       queryClient.invalidateQueries({ queryKey: ["cabins"] });
@@ -31,25 +25,8 @@ function CreateCabinForm({ cabinToEdit = {} }) {
     onError: (err) => toast.error(err.message),
   });
 
-  const { mutate: editCabin, isLoading: isEditing } = useMutation({
-    mutationFn: ({ newCabinData, id }) => createEditCabin(newCabinData, id),
-    onSuccess: () => {
-      toast.success("Cabin successfully edited");
-      queryClient.invalidateQueries({ queryKey: ["cabins"] });
-      reset();
-    },
-    onError: (err) => toast.error(err.message),
-  });
-
-  const isWorking = isCreating || isEditing;
   function onSubmit(data) {
-    const image = typeof data.image === "string" ? data.image : data.image[0];
-    if (isEditSession) {
-      editCabin({ newCabinData: { ...data, image }, id: editId });
-    } else {
-      createCabin({ ...data, image: data.image[0] });
-    }
-
+    mutate({ ...data, image: data.image[0] });
     // console.log(data);
   }
   function onError(errors) {
@@ -61,7 +38,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
         <Input
           type="text"
           id="name"
-          disabled={isWorking}
+          disabled={isCreating}
           {...register("name", {
             required: "This field is required",
           })}
@@ -72,7 +49,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
         <Input
           type="number"
           id="maxCapacity"
-          disabled={isWorking}
+          disabled={isCreating}
           {...register("maxCapacity", {
             required: "This field is required",
             min: {
@@ -87,7 +64,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
         <Input
           type="number"
           id="regularPrice"
-          disabled={isWorking}
+          disabled={isCreating}
           {...register("regularPrice", {
             required: "This field is required",
             min: {
@@ -102,12 +79,12 @@ function CreateCabinForm({ cabinToEdit = {} }) {
         <Input
           type="number"
           id="discount"
-          disabled={isWorking}
+          disabled={isCreating}
           defaultValue={0}
           {...register("discount", {
             required: "This field is required",
             validate: (value) =>
-              parseFloat(value) < parseFloat(getValues().regularPrice) ||
+              value < getValues().regularPrice ||
               "Discount should be less than regular price",
           })}
         />
@@ -120,7 +97,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
         <Textarea
           type="number"
           id="description"
-          disabled={isWorking}
+          disabled={isCreating}
           defaultValue=""
           {...register("description", {
             required: "This field is required",
@@ -133,7 +110,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
           id="image"
           accept="image/*"
           {...register("image", {
-            required: isEditSession ? false : "This field is required",
+            required: "This field is required",
           })}
         />
       </FormRow>
@@ -143,8 +120,8 @@ function CreateCabinForm({ cabinToEdit = {} }) {
         <Button variation="secondary" type="reset">
           Cancel
         </Button>
-        <Button $variation="primary" disabled={isWorking}>
-          {isEditSession ? "Edit cabin" : "Create new cabin"}
+        <Button $variation="primary" disabled={isCreating}>
+          Add cabin
         </Button>
       </FormRow>
     </Form>
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -9,47 +9,55 @@ export async function getCabins() {
   return data;
 }
 
-export async function createCabin(newCabin) {
+export async function createEditCabin(newCabin, id) {
   //https://doadxveldfyvhboixznn.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg
+  //console.log(newCabin, id);
+  //这里检查newCabin.image是否存在，并且图片路径是否以supabaseUrl开头。
+  //如果newCabin.image已经是一个完整的Supabase存储路径，说明这是一个已经上传过的图片，不需要重新生成路径
+
+  const hasImagePath = newCabin.image?.startsWith?.(supabaseUrl);
+  // 如果图片路径已存在且是来自Supabase的URL，并且用户没有上传新的图片，则跳过图片上传
+  const shouldUploadImage = !hasImagePath || typeof newCabin.image !== "string";
+
   const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
     "/",
     ""
   );
-  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+  //如果图片路径已经存在（hasImagePath为true），则使用原路径；否则，使用生成的新的imagePath。
+  const imagePath = hasImagePath
+    ? newCabin.image
+    : `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+  //1.create cabin only if there is no id
+  let query = supabase.from("cabins");
+  //a) create
+  if (!id) query = query.insert([{ ...newCabin, image: imagePath }]);
+  //b) editquery
 
-  const { data, error } = await supabase
-    .from("cabins")
-    .insert([{ ...newCabin, image: imagePath }])
-    .select();
+  //最终执行 await query.select().single() 时，才会真正执行数据库操作，并将结果存储到 data 和 error 中。
+  if (id) {
+    query = query.update({ ...newCabin, image: imagePath }).eq("id", id);
+  }
+  const { data, error } = await query.select().single();
+
   if (error) {
     console.error(error);
     throw new Error("Cabin could not be created");
   }
-  //upload files from javascript reference supabase doc
-  const { error: storageError } = await supabase.storage
-    .from("cabin-images")
-    .upload(imageName, newCabin.image);
-  // delete the cabin if there is an error
-  if (storageError) {
-    await supabase.from("cabins").delete().eq("id", data.id);
-    console.error(storageError);
-    throw new Error(
-      "Cabin image could not be uploaded and the cabin was not created"
-    );
+  //2. upload files from javascript reference supabase doc
+  if (shouldUploadImage) {
+    const { error: storageError } = await supabase.storage
+      .from("cabin-images")
+      .upload(imageName, newCabin.image);
+    // delete the cabin if there is an error
+    if (storageError) {
+      await supabase.from("cabins").delete().eq("id", data.id);
+      console.error(storageError);
+      throw new Error(
+        "Cabin image could not be uploaded and the cabin was not created"
+      );
+    }
   }
-  return data;
-}
 
-export async function updateCabin(cabin) {
-  const { data, error } = await supabase
-    .from("cabins")
-    .update({ other_column: "otherValue" })
-    .eq("some_column", "someValue")
-    .select();
-  if (error) {
-    console.error(error);
-    throw new Error("Cabin could not be updated");
-  }
   return data;
 }
 
